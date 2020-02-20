2e2461989dd9ec4bf1b6b339eb83de2c715e08f6
mc31 <mc31@MacBook-Pro-de-Nora.local>
2020-02-20 22:51:39 +0100

    Story 2

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # MarvelQuizz
+
+Story #01
+EN TANT QUE développeur
+JE VEUX afficher une question + image / son
+à l'utilisateur et proposer plusieurs réponses
+AFIN DE mettre en place mon UI
+
+DoD : Une Activity qui affiche une question etgroupe de réponses (RadioButton)
+
+-----------------------------------------------------------------------------------------------
+
+Story #02
+EN TANT QUE utilisateur
+JE VEUX choisir une réponse et valider mon choix
+AFIN DE tester mes connaissances
+
+DoD Un message affiche à l'utilisateur si réponseest la bonne==> Si la réponse est mauvaise, afficher la bonne réponse
+
+------------------------------------------------------------------------------------------------
--- app/src/main/java/marvelapp/quizz/MainActivity.kt
+++ app/src/main/java/marvelapp/quizz/MainActivity.kt
@@ -2,7 +2,9 @@ package marvelapp.quizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +21,29 @@ class MainActivity : AppCompatActivity() {
         secondResponse.text = "Aquaman"
         thirdResponse.text = "Thor"
 
-        //
+        fun toastMaker (message: String) {
+            val duration = Toast.LENGTH_LONG
+            var toast = Toast.makeText(applicationContext, message, duration)
+            return toast.show()
+        }
+
+        //Definition button action
+        //Evénement que l'on va déclencher au click
+        validateButton.setOnClickListener{
+            //initialisation du message de résultat
+            var message = "Réponse :"
+
+
+            // Vérification des réponses
+            if (firstResponse.isChecked) {
+                message += " Bravo !!! La bonne réponse est bien Batman !!!"
+                toastMaker(message)
+            } else {
+                message += " Dommage :( La bonne réponse est Batman"
+                toastMaker(message)
+            }
+
+        }
 
     }
 }
