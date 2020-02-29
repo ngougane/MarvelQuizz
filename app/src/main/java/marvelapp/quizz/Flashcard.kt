c22478d363fbd5d60f63de82167455816826d13d
mc31 <mc31@MacBook-Pro-de-Nora.local>
2020-02-29 23:34:02 +0100

    Story 5

--- app/src/main/java/marvelapp/quizz/Flashcard.kt
+++ app/src/main/java/marvelapp/quizz/Flashcard.kt
@@ -1,4 +1,16 @@
 package marvelapp.quizz
 
+class Flashcard(
+    val question: String = "Qui est ce personnage de Marvel",
+    val image_small: String, val image: String,
+    val responses: MutableList<String>,
+    val good_response: String ){
 
-data class Flashcard( val question : String = "Qui est ce personnage de Marvel" , val image_small : String , val image : String , val responses : MutableList<String> , val good_response : String)
+    //fonction qui affiche la question
+
+    //Fonction qui gére les boutons radio
+
+    //fonction qui gére la photo
+
+    //fonction qui gère la bonne réponse
+}
--- app/src/main/java/marvelapp/quizz/MainActivity.kt
+++ app/src/main/java/marvelapp/quizz/MainActivity.kt
@@ -1,176 +1,144 @@
 package marvelapp.quizz
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
-import android.widget.ImageView
 import android.widget.RadioButton
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
-    // instantiation Flashcard
-
-    var batman = Flashcard("Qui est-ce personnage de Marvel ?", "batman_small", "batman", mutableListOf("Batman", "Groot", "Robin"), "Batman")
-
-    var catwoman = Flashcard("De qui s'agit-il ? ", "batman_small", "batman", mutableListOf("Jessica Jones","Catwoman", "America Chavez"), "Catwoman")
-
-    var blackPanther = Flashcard("Mais qui est-ce personnage ?", "batman_small", "batman", mutableListOf("Black Panther", "Le fauve", "Drax"), "Black Panther")
-
-    var wolverine = Flashcard("Qui est ce superheroe ? ", "batman_small", "batman", mutableListOf("Colossus", "Drax", "Wolverine"), "Wolverine")
-
-
     // List of Flashcard object
+    var marvels = mutableListOf<Flashcard>()
 
-    var marvels = mutableListOf<Flashcard>(batman, catwoman,blackPanther,wolverine)
-
-    /*
-     * Function to initialize a message toast
-     * @parametre message : String
-     * @return toast
-     */
-
-    fun toastMaker (message: String) {
-        val duration = Toast.LENGTH_LONG
-        var toast = Toast.makeText(applicationContext, message, duration)
-        return toast.show()
-    }
+    var currentIndex: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // instantiation Flashcard
 
-        //call shuffle method to have a random marvel flashcard
-       // marvels.shuffle()
+        marvels.add(
+            Flashcard(
+                "Qui est-ce personnage de Marvel ?",
+                "batman_small",
+                "batman",
+                mutableListOf("Batman", "Groot", "Robin"),
+                "Batman"
+            )
+        )
+
+        marvels.add(
+            Flashcard(
+                "De qui s'agit-il ? ",
+                "batman_small",
+                "batman",
+                mutableListOf("Jessica Jones", "Catwoman", "America Chavez"),
+                "Catwoman"
+            )
+        )
+
+        marvels.add(
+            Flashcard(
+                "Mais qui est-ce personnage ?",
+                "batman_small",
+                "batman",
+                mutableListOf("Black Panther", "Le fauve", "Drax"),
+                "Black Panther"
+            )
+        )
+
+        marvels.add(
+            Flashcard(
+                "Qui est ce superheroe ? ",
+                "batman_small",
+                "batman",
+                mutableListOf("Colossus", "Drax", "Wolverine"),
+                "Wolverine"
+            )
+        )
+
+        showQuestion(marvels.get(currentIndex))
+
+        answerIsCorrect(marvels.get(currentIndex))
+
+    }
 
 
-        //initialization flashcard
-        var i = 0
+    /*
+  * Function to initialize a message toast
+  * @parametre message : String
+  * @return toast
+  */
 
-        questionText.text = marvels.elementAt(i).question
+    fun toastMaker(message: String) {
+        val duration = Toast.LENGTH_LONG
+        var toast = Toast.makeText(applicationContext, message, duration)
+        return toast.show()
+    }
+
+
+    fun showQuestion(flashcard: Flashcard) {
+
+        //Question initialization
+        questionText.text = flashcard.question
 
         //Text Button Radio initialization
-        var radiobuttonlist = marvels.elementAt(i).responses
+        var radiobuttonlist = flashcard.responses
+        createRadioButton(radiobuttonlist)
+
+    }
+
+    fun createRadioButton(responses: MutableList<String>) {
 
         //Call shuffle method to have random result
-        radiobuttonlist.shuffle()
+        responses.shuffle()
 
-        for (response in radiobuttonlist){
-
-            println("${response}")
-
+        //Adding radio button into radioGroup
+        for (response in responses) {
             var radioButton = RadioButton(this)
             radioButton.text = response
             radioGroup.addView(radioButton)
         }
+    }
 
+    fun answerIsCorrect(flashcard: Flashcard) {
 
+        //answer verification
+        validateButton.setOnClickListener {
+            //initialization message toast
+            var message = "Réponse :"
+            //I will check in the radioGroup if a radio button has been checked
+            val checkedRadioButtonId = radioGroup.checkedRadioButtonId
+            val selectedRadioButton = radioGroup.findViewById<RadioButton>(checkedRadioButtonId)
 
-        for(marvel in marvels){
+            // If none is checked it returns -1
+            // If it is checked I compared the value to the right answer
+            if (selectedRadioButton.text == flashcard.good_response) {
+                message += " Bravo !!! La bonne réponse est bien ${flashcard.good_response} !!!"
+                toastMaker(message)
+                //Transform the button
+                validateButton.text = "Question suivante"
+            } else {
+                message += " Dommage :( La bonne réponse est ${flashcard.good_response}"
+                toastMaker(message)
+            }
 
-            println(" avant :  ${i} ")
-
-            validateButton.setOnClickListener{
-
-                validateButton.text = "Valider"
-
-
-                    if (i  < marvels.count()){
-
-                        //answer verification
-                        validateButton.setOnClickListener{
-                            //initialization message toast
-                            var message = "Réponse :"
-                            //I will check in the radioGroup if a radio button has been checked
-                            val checkedRadioButtonId = radioGroup.checkedRadioButtonId
-                            val selectedRadioButton = radioGroup.findViewById<RadioButton>(checkedRadioButtonId)
-
-                            // If none is checked it returns -1
-                            // If it is checked I compared the value to the right answer
-
-                            if (selectedRadioButton.text ==  marvels.elementAt(i).good_response) {
-
-                                message += " Bravo !!! La bonne réponse est bien ${marvels.elementAt(i).good_response} !!!"
-                                toastMaker(message)
-
-                                //Transform the button
-                                validateButton.text = "Question suivante"
-
-                                i++
-
-                                println(" après :  ${i} ")
-
-
-                            } else {
-                                message += " Dommage :( La bonne réponse est ${marvels.elementAt(i).good_response}"
-                                toastMaker(message)
-                        }
-
-                    }
-
-                }
-
+            //Going to the next question
+            currentIndex++
+            //I check if there are other questions
+            if(currentIndex >= marvels.size){
+                println("c'est terminé")
+            }else{ // we going to the next question
+                radioGroup.removeAllViews()
+                showQuestion(marvels.get(currentIndex))
             }
 
         }
-
-
-
-        // text field initialization
-       // questionText.text = first.question
-
-        //Set a view full on click to the image view
-
-           // quizzImage.setOnClickListener{
-              //  println("click !!!")
-               // quizzImage.setImageResource(R.drawable.batman_small)
-
-
-           // }
-
-        //Initialization data
-       // var responses = first.responses
-        //Call shuffle method to have random result
-       // responses.shuffle()
-
-       // for (response in responses){
-
-         //   var radioButton = RadioButton(this)
-           // radioButton.text = response
-         //   radioGroup.addView(radioButton)
-
-       // }
-
-          //Definition button action
-            //  validateButton.setOnClickListener{
-            //message initialization
-            //  var message = "Réponse :"
-              //I will check in the radioGroup if a radio button has been checked
-             // val checkedRadioButtonId = radioGroup.checkedRadioButtonId
-             // val selectedRadioButton = radioGroup.findViewById<RadioButton>(checkedRadioButtonId)
-
-              // If none is checked it returns -1
-              //If it is checked I compared the value to the right answer
-            //  if (selectedRadioButton.text ==  first.good_response) {
-             //   message += " Bravo !!! La bonne réponse est bien Batman !!!"
-             //   toastMaker(message)
-              //Transform the button
-              //  validateButton.text = "Question suivante"
-               // questionText.text = second.question
-
-
-
-              //} else {
-               // message += " Dommage :( La bonne réponse est Batman"
-               // toastMaker(message)
-             // }
-
-      // }
-
     }
+
 }
